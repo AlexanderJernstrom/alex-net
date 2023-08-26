f02265d41ec643e94d95d0c3fbb2f7f0d323183c
Alexander Jernström <alexanderjernstrom@gmail.com>
2023-08-26 16:37:16 +0200

    Started on linreg function

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 double derivative(double x1, double x2);
 double simpleLinReg(double xValues[], double yValues[], int n);
 double mse(double x[], double y[], int n, double slope);
+double *linearRegression(double xValues[], double yValues[], int n);
+double **matMul(double **matrix1, double **matrix2, int m, int n, int k);
+double **matInv(double **matrix, int n);
 
 double derivative(double x1, double x2)
 {
@@ -45,6 +48,66 @@ double simpleLinReg(double xValues[], double yValues[], int n)
     return slope;
 }
 
+double **matInv(double **matrix, int n)
+{
+}
+
+// Linear regression, fits a line to data, returns slope and y-intercept [slope, y-intercept]
+double *linearRegression(double xValues[], double yValues[], int n)
+{
+    if (sizeof(xValues) != sizeof(yValues))
+    {
+        printf("Error: Dimensions not matching\n");
+        return NULL;
+    }
+
+    double xSquaredSum = 0.0;
+    double xSum = 0.0;
+    double xyProductSum = 0.0;
+    double ySum = 0.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        xSquaredSum += xValues[i] * xValues[i];
+        xSum += xValues[i];
+        xyProductSum += xValues[i] * yValues[i];
+        ySum += yValues[i];
+    }
+
+    double matrix1[1][1] = {{xyProductSum}, {ySum}};
+    double matrix2[2][2] = {{xSquaredSum, xSum}, {xSum, n}};
+}
+
+// Matrix multiplication of MxN with NxK, returns the result of the multiplication
+double **matMul(double **matrix1, double **matrix2, int m, int n, int k)
+{
+    if (sizeof(matrix1[0]) != sizeof(matrix2))
+    {
+        printf("Error: Matrix dimensions do not match\n");
+        return NULL;
+    }
+
+    double **result = malloc(m * sizeof(double *));
+    for (int i = 0; i < m; i++)
+    {
+        result[i] = malloc(k * sizeof(double));
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            result[i][j] = 0;
+            for (int l = 0; l < n; l++)
+            {
+                result[i][j] += matrix1[i][l] * matrix2[l][j];
+            }
+        }
+    }
+
+    return result;
+}
+
 int main()
 {
     printf("Hello World!\n");
@@ -52,6 +115,7 @@ int main()
     double xValues[] = {1, 2, 3, 4, 5};
     double yValues[] = {2, 4, 6, 8, 10};
     // Should get k = 2
+
     printf("k = %f\n", simpleLinReg(xValues, yValues, 5));
 
     return 0;
